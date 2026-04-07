7d4928bc4ba9f60cd1f728cf4caa066dc12d1162
s0196026 <elizavetagolodkova@gmail.com>
2026-04-07 21:10:48 +0300

    Update form.php

--- BE3/form.php
+++ BE3/form.php
@@ -10,3 +10,19 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     include('index.php');
     exit();
 }
+/*
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+header('Content-Type: text/html; charset=UTF-8');
+
+// Обрабатываем и GET, и POST
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    include('index.php');  // обработка данных
+} elseif ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    if (!empty($_GET['save'])) {
+        print('Спасибо, результаты сохранены.');
+    }
+    include('index.php');  // показ формы
+}
+*/
