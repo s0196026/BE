1d366bdd7f5fdf2a48f6bacb68ba3c9c9b4ca711
s0196026 <elizavetagolodkova@gmail.com>
2026-04-07 18:13:26 +0300

    Update form.php

--- BE3/form.php
+++ BE3/form.php
@@ -6,7 +6,7 @@ header('Content-Type: text/html; charset=UTF-8');
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     if (!empty($_GET['save'])) {
-        print('Спасибо, результаты сохранены.');
+        print('Сохранено');
     }
     include('index.php');
     exit();
