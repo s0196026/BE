1cdc8a23ee5bc9f1e912913fea2bfcbd3d941679
s0196026 <elizavetagolodkova@gmail.com>
2026-04-20 01:45:41 +0300

    Update register.php

--- BE5/register.php
+++ BE5/register.php
@@ -2,7 +2,7 @@
 session_start();
 
 // Подключение к БД
-$db = new PDO("mysql:host=localhost;dbname=bd_be5", 'u82388', '2008675', [
+$db = new PDO("mysql:host=localhost;dbname=bd_be5", 'u82388', '57680025', [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]);
 
