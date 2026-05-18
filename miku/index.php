77c3183a966ca4e9e90431b8d880893fe87bfe09
s0196026 <elizavetagolodkova@gmail.com>
2026-05-18 22:41:51 +0300

    Update and rename index.html to index.php

--- miku/index.php
+++ miku/index.php
@@ -28,6 +28,7 @@
                             </select>
                         </label>
                         <p class="CI"><a href="#form">Поделитесь мнением!</a></p>
+                        <p class="CI"><b>Войти</b></p>
                     </nav>
                 </div>
             </div>
