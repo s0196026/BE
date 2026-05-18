756be0d5603d0e070f3cfd21fe14a0c1495833a1
s0196026 <elizavetagolodkova@gmail.com>
2026-05-18 23:33:30 +0300

    Update index.php

--- miku/index.php
+++ miku/index.php
@@ -28,7 +28,7 @@
                             </select>
                         </label>
                         <p class="CI"><a href="#form">Поделитесь мнением!</a></p>
-                        <p class="CI">< a href="login.php"><b>Войти</b></a></p>
+                        <p class="CI"><b><a href="login.php">Войти</a></b></p>
                     </nav>
                 </div>
             </div>
